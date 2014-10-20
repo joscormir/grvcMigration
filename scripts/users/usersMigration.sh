0c99be4c029424772b42a3d6ecba7dc938814a4e
joscormir <joscormir@gmail.com>
2014-10-20 16:20:59 +0200

    Edited usersMigration.sh

--- scripts/users/usersMigration.sh
+++ scripts/users/usersMigration.sh
@@ -1,9 +1,28 @@
 #! /bin/bash 
 
 #En este script se va a copiar los datos de los usuarios previa comprobación  de que no hay conflicto.
+
+awk -v -F: 'FNR == NR {a[$3]; next} $3 in a'  /root/passwd.old /etc/passwd
+ 
+#Si no existe conflicto podemos pasar a realizar copia de los usuarios.
+
 let limit = 
 
-awk -v LIMIT=$limit -F: '($3>=LIMIT) && ($3!=65534)'  /root/passwd.old #/etc/passwd
- 
+#----------------usuarios ---------------------------------#
 
+/root/passwd.old > /etc/passwd
+
+#----------------grupos -----------------------------------#
+
+/root/group.old > /etc/group
+
+#Hacemos lo mismo para el fichero de shadow con las contraseñas encriptadas, usando el mismo límite:
+
+#----------------passwords --------------------------------#
+
+/root/shadow.old > /etc/shadow
+
+#----------------group passwords --------------------------#
+
+/root/gshadow.old > /etc/gshadow
 
