a9aeb34433ef7afe1bfc24b7b9c13ac65b28f299
joscormir <joscormir@gmail.com>
2015-03-17 21:23:17 +0100

    Funcionando script filesToMigrate.sh

--- scripts/system/filesToMigrate.sh
+++ scripts/system/filesToMigrate.sh
@@ -2,28 +2,23 @@
 
 #Define variables to use in the script
 
-lastDate 	= "2014-09-19"
-actualDate 	= "2015-03-17"
+lastDate="2014-09-19"
+actualDate="2015-03-17"
 
-dirToSave = "/home/joscormir/Documentos/grvcMigration/scripts/system/"
+dirToSave="/home/joscormir"
 
 clear
+
 echo "Extracting files that have changed in /home"
-
 cd /home 
-find . type f -newermt $lastDate ! -newermt $actualDate >> $dirtoSave/homeChanges.txt
+find . -type f -newermt $lastDate ! -newermt $actualDate >> $dirToSave/homeChanges.txt
 
-clear
 echo "Extracting files that have changed in /disk"
-
 cd /disk 
-find . type f -newermt $lastDate ! -newermt $actualDate >> $dirtoSave/diskChanges.txt
+find . -type f -newermt $lastDate ! -newermt $actualDate >> $dirToSave/diskChanges.txt
 
-
-clear
 echo "Extracting files that have changed in /var"
-
 cd /var 
-find . type f -newermt $lastDate ! -newermt $actualDate >> $dirtoSave/varChanges.txt
+find . -type f -newermt $lastDate ! -newermt $actualDate >> $dirToSave/varChanges.txt
 
 echo "Finished"
